852bc3240c0620e95c849214254a956f26fcf472
James Wang <jameswang9909@hotmail.com>
2016-12-16 11:45:24 -0500

    Fix pipes

--- pipe_networking.c
+++ pipe_networking.c
@@ -12,23 +12,26 @@ int server_handshake(int *from_client) {
     printf("[*] Waiting for client connection\n");
 
     int wkp = open(WKP, O_RDONLY);
-    char buf[MESSAGE_BUFFER_SIZE];
-    read(wkp, buf, sizeof(buf));
-    printf("[*] Received: %s\n", buf);
+    char pp[64];
+    read(wkp, pp, sizeof(pp));
+    printf("[*] Received: %s\n", pp);
 
-    int private = open(buf, O_WRONLY);
+    int private = open(pp, O_WRONLY);
     char *message = "syn-ack";
     write(private, message, sizeof(message));
     close(private);
 
-    private = open(buf, O_RDONLY);
+    char buf[MESSAGE_BUFFER_SIZE];
+    private = open(pp, O_RDONLY);
     read(private, buf, sizeof(buf));
-    printf("[*] Processing: %s\n", buf);
+    printf("[*] Received: %s\n", buf);
+    close(private);
 
-    mkfifo("p1", 0644);
-    mkfifo("p2", 0644);
-    int to_client = open("p1", O_WRONLY);
-    *from_client = open("p2", O_RDONLY);
+    printf("Opening %s\n", pp);
+    int to_client = open(pp, O_WRONLY);
+
+    *from_client = wkp;
+    remove(pp);
 
     return to_client;
 }
@@ -54,15 +57,10 @@ int client_handshake(int *to_server) {
     private = open(buf, O_WRONLY);
     write(private, message, sizeof(message));
 
-    remove(buf);
+    printf("Opening %s\n", buf);
+    int from_server = open(buf, O_RDONLY);
+    *to_server = wkp;
     remove(WKP);
-
-    close(wkp);
-
-    mkfifo("p1", 0644);
-    mkfifo("p2", 0644);
-    int from_server = open("p1", O_RDONLY);
-    *to_server = open("p2", O_WRONLY);
 
     return from_server;
 }
