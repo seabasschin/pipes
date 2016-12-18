8f44f24a3172cc3697b0cfe64fb6e4e132f62464
James Wang <jameswang9909@hotmail.com>
2016-12-18 15:03:02 -0500

    Implement forking

--- pipe_networking.c
+++ pipe_networking.c
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
 
 #include "pipe_networking.h"
 
@@ -38,19 +39,47 @@ int client_handshake(int *to_server) {
     printf("[*] Connecting to server\n");
 
     int wkp = open(WKP, O_WRONLY);
-    *to_server = wkp;
     write(wkp, buf, sizeof(buf));
 
     int private = open(buf, O_RDONLY);
     char received[MESSAGE_BUFFER_SIZE];
     read(private, received, sizeof(received));
     printf("[*] Received: %s\n", received);
+    if (remove(buf) == -1) {
+        printf("Error: %s\n", strerror(errno));
+    }
 
     char *message = "ack";
     write(wkp, message, sizeof(message));
 
     *to_server = wkp;
-    remove(WKP);
+
+    return private;
+}
+
+int server_handshake1(char *buf) {
+    mkfifo(WKP, 0644);
+    printf("[*] Waiting for client connection\n");
+
+    int wkp = open(WKP, O_RDONLY);
+    read(wkp, buf, sizeof(buf));
+    printf("[*] Received: %s\n", buf);
+    if (remove(WKP) == -1) {
+        printf("Error: %s\n", strerror(errno));
+    }
+
+    return wkp;
+}
+
+int server_handshake2(char *buf, int from_client) {
+    int private = open(buf, O_WRONLY);
+    char *message = "syn-ack";
+    write(private, message, sizeof(message));
+
+    char received[MESSAGE_BUFFER_SIZE];
+    read(from_client, received, sizeof(received)); // hack
+    read(from_client, received, sizeof(received));
+    printf("[*] Received: '%s'\n", received);
 
     return private;
 }
--- pipe_networking.h
+++ pipe_networking.h
@@ -1,5 +1,9 @@
-#define MESSAGE_BUFFER_SIZE 1028
+#define MESSAGE_BUFFER_SIZE 1024
+#define HANDSHAKE_BUFFER_SIZE 1024
 #define WKP "wkp"
 
 int client_handshake(int *to_client);
 int server_handshake(int *from_client);
+
+int server_handshake1(char *buf);
+int server_handshake2(char *buf, int from_client);
